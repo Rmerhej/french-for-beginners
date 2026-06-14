f1663a181392bc9c129edbd4c5b79d44e9f624b9
Rmerhej <rolandmerhej8@gmail.com>
2026-06-14 18:56:26 +0200

    modif WebConfig

--- src/main/java/com/apprendrefr/config/WebConfig.java
+++ src/main/java/com/apprendrefr/config/WebConfig.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Value("${upload.path:uploads/}")
-    private String uploadPath;
+    //@Value("${upload.path:uploads/}")
+    //private String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
