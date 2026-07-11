96ee1e27e37bf41ede6582b2871f0785b37d990a
Rmerhej <rolandmerhej8@gmail.com>
2026-07-11 20:17:38 +0200

    création de 'Quiz culture française' ,culture.html ,redirection ds ExerciseController

--- src/main/java/com/apprendrefr/controller/ExerciseController.java
+++ src/main/java/com/apprendrefr/controller/ExerciseController.java
@@ -1,9 +1,16 @@
 package com.apprendrefr.controller;
 
+import com.apprendrefr.entity.Exercise;
 import com.apprendrefr.service.ExerciseService;
+import com.apprendrefr.service.LessonService;
 import com.apprendrefr.service.ScoreService;
 import com.apprendrefr.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ExerciseController {
@@ -11,13 +18,22 @@ public class ExerciseController {
     private final ExerciseService exerciseService;
     private final UserService userService;
     private final ScoreService scoreService;
+    private final LessonService lessonService;
 
     // injection automatique des 3 services
     public ExerciseController(ExerciseService exerciseService,
                               UserService userService,
-                              ScoreService scoreService) {
+                              ScoreService scoreService, LessonService lessonService) {
         this.exerciseService = exerciseService;
         this.userService = userService;
         this.scoreService = scoreService;
+        this.lessonService = lessonService;
+    }
+
+    @GetMapping("/culture")
+    public String getCulturePage(Model model) {
+        List<Exercise> exercises = exerciseService.findByLessonTitleContaining("Culture");
+        model.addAttribute("exercises", exercises != null ? exercises : new ArrayList<>());
+        return "culture";
     }
 }
